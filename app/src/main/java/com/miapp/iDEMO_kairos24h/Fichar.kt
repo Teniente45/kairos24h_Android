5dbf2d7d98aac4ab66f6f3df7962154662214bee
Teniente45 <Juanteniente45@gmail.com>
2025-03-05 13:15:42 +0100

    iDEMO. Colocando botones en MainActivity

--- app/src/main/java/com/miapp/iDEMO_kairos24h/Fichar.kt
+++ app/src/main/java/com/miapp/iDEMO_kairos24h/Fichar.kt
@@ -1,34 +1,24 @@
 package com.miapp.iDEMO_kairos24h
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.webkit.CookieManager
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.ImageView
-import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Switch
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.bumptech.glide.Glide
@@ -40,16 +30,13 @@ import kotlinx.coroutines.delay
 class Fichar : ComponentActivity() {
 
     private val handler = Handler(Looper.getMainLooper())
-    // 2 horas en milisegundos
     private val sessionTimeoutMillis = 2 * 60 * 60 * 1000L
     private var lastInteractionTime = System.currentTimeMillis()
-    // Variable para almacenar el WebView creado en Compose
     private var webView: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Obtener credenciales usando la función adecuada según la versión del SDK
         val (storedUser, storedPassword) = if (android.os.Build.VERSION.SDK_INT == 23) {
             AuthManager.getUserCredentialsPair(this)
         } else {
@@ -62,20 +49,15 @@ class Fichar : ComponentActivity() {
             return
         }
 
-        // Usamos las credenciales del Intent, o las almacenadas
         val usuario = intent.getStringExtra("usuario") ?: storedUser
         val password = intent.getStringExtra("password") ?: storedPassword
 
         setContent {
-            FicharScreen(
-                usuario = usuario,
-                password = password
-            )
+            FicharScreen(usuario = usuario, password = password)
         }
 
         startActivitySimulationTimer()
     }
-
     private fun startActivitySimulationTimer() {
         handler.postDelayed(object : Runnable {
             override fun run() {
@@ -118,7 +100,6 @@ class Fichar : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        // Comprobamos nuevamente las credenciales
         val (storedUser, storedPassword, _) = AuthManager.getUserCredentials(this)
         if (storedUser.isEmpty() || storedPassword.isEmpty()) {
             navigateToLogin()
@@ -166,94 +147,33 @@ class Fichar : ComponentActivity() {
         finish()
     }
 }
-
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun FicharScreen(usuario: String, password: String) {
     var isLoading by remember { mutableStateOf(true) }
 
-    LaunchedEffect(true) {
+    LaunchedEffect(Unit) {
         delay(6000)
         isLoading = false
     }
 
     Box(modifier = Modifier.fillMaxSize()) {
-        AndroidView(
-            factory = { context ->
-                WebView(context).apply {
-                    val handler = Handler(Looper.getMainLooper())
-                    var loginTimeoutRunnable: Runnable? = null
-
-                    settings.javaScriptEnabled = true
-                    settings.setSupportZoom(true)
-                    settings.builtInZoomControls = true
-                    settings.displayZoomControls = false
-
-                    webViewClient = object : WebViewClient() {
-                        override fun onPageStarted(view: WebView?, url: String?, favicon: android.graphics.Bitmap?) {
-                            super.onPageStarted(view, url, favicon)
-                            if (url != WebViewURL.LOGIN_URL && loginTimeoutRunnable != null) {
-                                handler.removeCallbacks(loginTimeoutRunnable!!)
-                                loginTimeoutRunnable = null
-                            }
-                        }
-
-                        override fun onPageFinished(view: WebView?, url: String?) {
-                            super.onPageFinished(view, url)
-                            if (url == WebViewURL.LOGIN_URL) {
-                                loginTimeoutRunnable = Runnable {
-                                    clearCookiesAndClearCredentials(view)
-                                }
-                                handler.postDelayed(loginTimeoutRunnable!!, 4000)
-                            } else {
-                                if (loginTimeoutRunnable != null) {
-                                    handler.removeCallbacks(loginTimeoutRunnable!!)
-                                    loginTimeoutRunnable = null
-                                }
-                            }
-                            // Inyectar JavaScript para verificar si existe el div de error
-                            view?.evaluateJavascript(
-                                """
-        (function() {
-            var errorDiv = document.getElementById('LoginForm_password_em_');
-            return errorDiv ? errorDiv.innerText : "";
-        })();
-        """.trimIndent()
-                            ) { result ->
-                                // Si se detecta el mensaje de error, se ejecuta clearCookiesAndClearCredentials
-                                if (result.contains("Usuario o Password") && result.contains("incorrectos")) {
-                                    clearCookiesAndClearCredentials(view)
-                                } else {
-                                    // Si no se detecta error, se inyecta el formulario para enviar las credenciales
-                                    view.evaluateJavascript(
-                                        """
-                                                (function() {
-                                                    document.getElementsByName('LoginForm[username]')[0].value = '$usuario';
-                                                    document.getElementsByName('LoginForm[password]')[0].value = '$password';
-                                                    document.querySelector('form').submit();
-                                                })();
-                                                """.trimIndent(),
-                                        null
-                                    )
-                                }
-                            }
-                        }
-
-
-                        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                            val url = request?.url.toString()
-                            if (url.contains("site/logout")) {
-                                clearCookiesAndClearCredentials(view)
-                                return true
-                            }
-                            return super.shouldOverrideUrlLoading(view, request)
-                        }
+        Column(modifier = Modifier.fillMaxSize()) {
+            AndroidView(
+                factory = { context ->
+                    WebView(context).apply {
+                        settings.javaScriptEnabled = true
+                        settings.setSupportZoom(true)
+                        settings.builtInZoomControls = true
+                        settings.displayZoomControls = false
+                        loadUrl(WebViewURL.LOGIN)
                     }
-                    loadUrl(WebViewURL.LOGIN_URL)
-                }
-            },
-            modifier = Modifier.fillMaxSize()
-        )
+                },
+                modifier = Modifier.weight(1f)
+            )
+
+            BottomNavigationBar()
+        }
 
         if (isLoading) {
             Box(
@@ -270,10 +190,6 @@ fun FicharScreen(usuario: String, password: String) {
                             .load(R.drawable.version_2)
                             .apply(RequestOptions().placeholder(R.drawable.version_2))
                             .into(imageView)
-                        imageView.layoutParams = RelativeLayout.LayoutParams(
-                            RelativeLayout.LayoutParams.WRAP_CONTENT,
-                            RelativeLayout.LayoutParams.WRAP_CONTENT
-                        )
                         imageView
                     },
                     modifier = Modifier.size(100.dp)
@@ -283,23 +199,44 @@ fun FicharScreen(usuario: String, password: String) {
     }
 }
 
-private fun clearCredentials(context: Context) {
-    val sharedPreferences = context.getSharedPreferences("UserSession", Context.MODE_PRIVATE)
-    with(sharedPreferences.edit()) {
-        remove("usuario")
-        remove("password")
-        apply()
+@Composable
+fun BottomNavigationBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        var isChecked by remember { mutableStateOf(false) }
+        Switch(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it }
+        )
+
+        NavigationButton("Fichaje", WebViewURL.Fichaje, R.drawable.icon_fichaje)
+        NavigationButton("Incidencia", WebViewURL.Incidencia, R.drawable.icon_incidencia)
+        NavigationButton("Horarios", WebViewURL.Horarios, R.drawable.icon_horarios)
     }
 }
 
-private fun clearCookiesAndClearCredentials(view: WebView?) {
-    val cookieManager = CookieManager.getInstance()
-    cookieManager.removeAllCookies(null)
-    cookieManager.flush()
-    clearCredentials(view?.context ?: return)
-    view.context?.let {
-        val intent = Intent(it, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        it.startActivity(intent)
+@Composable
+fun NavigationButton(text: String, url: String, iconResId: Int) {
+    val context = LocalContext.current
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        IconButton(
+            onClick = {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                context.startActivity(intent)
+            }
+        ) {
+            Icon(
+                painter = painterResource(id = iconResId),
+                contentDescription = text,
+                modifier = Modifier.size(32.dp)
+            )
+        }
+        Text(text)
     }
 }
--- app/src/main/java/com/miapp/iDEMO_kairos24h/enlaces_internos/URLs.kt
+++ app/src/main/java/com/miapp/iDEMO_kairos24h/enlaces_internos/URLs.kt
@@ -4,30 +4,32 @@ package com.miapp.iDEMO_kairos24h.enlaces_internos
 // Estás son las URL que se nos mostrarán en el WebView
 object WebViewURL {
     /*==================================================================*/
-    const val PRODUCCION_BEIMAN = "https://beimancpp.tucitamedica.es/index.php?r=site/login&0%5BxEntidad%5D="
-    const val forgotPassword_BEIMAN = "https://beimancpp.tucitamedica.es/index.php?r=site/solicitudRestablecerClave"
+    const val URL_BEIMAN = "https://beimancpp.tucitamedica.es/index.php?"
+    const val URL_Kairos24h = "https://controlhorario.kairos24h.es/index.php?"
+    const val URL_DEMO_Kairos24h = "https://democontrolhorario.kairos24h.es/index.php?"
     /*==================================================================*/
-    const val PRODUCCION_Kairos24h = "https://controlhorario.kairos24h.es/index.php?r=site/login&0%5BxEntidad%5D="
-    const val DEMO_Kairos24h = "https://democontrolhorario.kairos24h.es/index.php?r=site/login&0%5BxEntidad%5D="
-    const val forgotPassword_Kairos24h = "https://www.controlhorario.kairos24h.es/index.php?r=site/solicitudRestablecerClave"
-    /*==================================================================*/
-
+    const val URL_USADA = URL_DEMO_Kairos24h
     /*--------------------------------------------------------*/
-    const val LOGIN_URL = DEMO_Kairos24h
-    const val forgotPassword = forgotPassword_Kairos24h
+    const val LOGIN = URL_USADA + "r=site/login&0%5BxEntidad%5D="
+    const val forgotPassword = URL_USADA + "r=site/solicitudRestablecerClave"
+    const val Inicio = URL_USADA + "r=site/login&xEntidad=1002&cApp=APP_CPP"
+    const val Fichaje = URL_USADA + "r=explotacion/consultarExplotacion&cTipExp=FICHAJE"
+    const val Incidencia =
+        URL_USADA + "r=explotacion/consultarExplotacion&cTipExp=INCIDENCIA&cOpcionVisual=INCBAN"
+    const val Horarios =
+        URL_USADA + "r=explotacion/consultarExplotacion&cTipExp=HORARIO&cModoVisual=HORMEN"
     /*--------------------------------------------------------*/
 }
 
 // Esta será la URL que construiremos cuando desde el login de nuestra APK introduzcamos el Usuario y la Contraseña
 object BuildURL {
     /*==================================================================*/
-    const val PRODUCCION_BEIMAN = "https://beimancpp.tucitamedica.es/index.php?r=wsExterno/"
+    const val URL_BEIMAN = "https://beimancpp.tucitamedica.es/index.php?"
+    const val URL_Kairos24h = "https://controlhorario.kairos24h.es/index.php?"
+    const val URL_DEMO_Kairos24h = "https://democontrolhorario.kairos24h.es/index.php?"
     /*==================================================================*/
-    const val PRODUCCION_Kairos24h = "https://controlhorario.kairos24h.es/index.php?r=wsExterno/"
-    const val DEMO_Kairos24h = "https://democontrolhorario.kairos24h.es/index.php?r=wsExterno/"
-    /*==================================================================*/
-
+    const val URL_USADA = URL_DEMO_Kairos24h
     /*--------------------------------------------------------*/
-    const val LOGIN_URL = DEMO_Kairos24h
+    const val LOGIN = URL_USADA + "r=wsExterno/"
     /*--------------------------------------------------------*/
 }
